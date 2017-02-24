c59fade68ba7cc4187d49ff046f59c8cd9c82dc1
gustavdelius <gustav.delius@gmail.com>
2017-02-24 16:11:30 +0000

    Now we have a choice between using persp3d and persp3D

--- planktr/R/plot3_D.R
+++ planktr/R/plot3_D.R
@@ -7,10 +7,10 @@
 #' @param xs Log size of species to plot. If there is no species at this size,
 #' choose the next larger species, or if there is none, the largest species.
 #' @include sim.R
-setGeneric("plot3d", function(r, t, xs, ...) {standardGeneric("plot3d")})
+setGeneric("plot3D", function(r, t, xs, ...) {standardGeneric("plot3D")})
 
-#' @describeIn plot3d 3d plot of population density against x and xa at given time
-setMethod("plot3d", c(r="Sim", t="numeric", xs="missing"),
+#' @describeIn plot3D 3D plot of population density against x and xa at given time
+setMethod("plot3D", c(r="Sim", t="numeric", xs="missing"),
     function(r, t, zlog=FALSE) {
         ti <- which(r@t >= t)[1]
         if (is.na(ti)) {
@@ -19,14 +19,15 @@ setMethod("plot3d", c(r="Sim", t="numeric", xs="missing"),
         }
         z <- if (zlog) log(pmax(r@p[ti, , ], 0.05)) else r@p[ti, , ]
         zlab <- if (zlog) "log(p)" else "p"
-        persp3d(r@x, r@xs, z, col = "lightblue",
+        persp3D(r@x, r@xs, z,
                 xlab="x", ylab="x*", zlab=zlab,
-                main=paste("Spectrum at t=", r@t[ti]))
+                main=paste("Spectrum at t=", r@t[ti]),
+                ticktype="detailed", nticks=4)
     }
 )
 
-#' @describeIn plot3d 3d plot of population density of given species against t and x
-setMethod("plot3d", c(r="Sim", t="missing", xs="numeric"),
+#' @describeIn plot3D 3D plot of population density of given species against t and x
+setMethod("plot3D", c(r="Sim", t="missing", xs="numeric"),
     function(r, xs, zlog=FALSE) {
         xsi <- which(r@xs >= xs)[1]
         if (is.na(xsi)) {
@@ -35,21 +36,23 @@ setMethod("plot3d", c(r="Sim", t="missing", xs="numeric"),
         }
         z <- if (zlog) log(pmax(r@p[ , , xsi], 0.05)) else r@p[ , , xsi]
         zlab <- if (zlog) "log(p)" else "p"
-        persp3d(r@t, r@x, z, col = "lightblue",
+        persp3D(r@t, r@x, z,
                 xlab="t", ylab="x", zlab=zlab,
-                main=paste("Spectrum at xs=", r@xs[xsi]))
+                main=paste("Spectrum at xs=", r@xs[xsi]),
+                ticktype="detailed", nticks=4)
 
     }
 )
 
-#' @describeIn plot3d 3d plot of community density against x and xa
-setMethod("plot3d", c(r="Sim", t="missing", xs="missing"),
-          function(r, zlog=FALSE) {
-              com <- get_community(r)
+#' @describeIn plot3D 3D plot of community density against x and xa
+setMethod("plot3D", c(r="Sim", t="missing", xs="missing"),
+          function(r, zlog=FALSE, smooth=TRUE) {
+              com <- get_community(r, smooth = smooth)
               com <- if (zlog) log(pmax(com, 0.05)) else com
               zlab <- if (zlog) "log(p_c)" else "p_c"
-              persp3d(r@t, r@xa, com, col = "lightblue",
-                      xlab="t", ylab="xa", zlab=zlab,
-                      main="Community size spectrum")
+              persp3D(r@t, r@xa, com,
+                      xlab="t", ylab="x", zlab=zlab,
+                      main="Community size spectrum",
+                      ticktype="detailed", nticks=4)
           }
 )
